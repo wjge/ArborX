257f8b8252719f54c3d0667198100121d4df302e
w0g <gew1@ornl.gov>
2020-10-17 00:14:33 -0400

    add another box position for testing

--- test/tstDetailsRay.cpp
+++ test/tstDetailsRay.cpp
@@ -121,58 +121,89 @@ BOOST_AUTO_TEST_CASE(intersects_box)
   using ArborX::Ray;
   using ArborX::Details::intersects;
 
-  Box unit_box{{0, 0, 0}, {1, 1, 1}};
+  Box unit_box1{{0, 0, 0}, {1, 1, 1}};
+  Box unit_box2{{-1, -1, -1}, {0, 0, 0}};
 
+  // box1
   // origin is within the box
-  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {0, 1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {0, 0, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 1, 1}}, unit_box));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {0, 1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {0, 0, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {1, 1, 1}}, unit_box1));
 
   // origin is outside the box
   // hit the center of the face
-  BOOST_TEST(intersects(Ray{{-1, .5, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, .5, .5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, .5, .5}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, .5, .5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, .5, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, .5, .5}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, 1.5, .5}, {1, -1, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1.5, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, 1.5, 1.5}, {1, -1, -1}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, -1, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, 0, -1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, -.5, .5}, {-1, 1, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, -.5, .5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, -.5, 1.5}, {-1, 1, -1}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 1, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 0, -1}}, unit_box));
+  BOOST_TEST(intersects(Ray{{-1, .5, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, .5, .5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, .5, .5}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, .5, .5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, .5, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, .5, .5}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, 1.5, .5}, {1, -1, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1.5, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, 1.5, 1.5}, {1, -1, -1}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, -1, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1.5, 1.5}, {1, 0, -1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, -.5, .5}, {-1, 1, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, -.5, .5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, -.5, 1.5}, {-1, 1, -1}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 1, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, -.5, 1.5}, {-1, 0, -1}}, unit_box1));
   // hit the center of an edge
-  BOOST_TEST(intersects(Ray{{-1, 0, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 0, .5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 0, .5}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, 0, .5}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, 0, .5}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, 0, .5}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, -1, .5}, {1, 1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, 1, .5}, {1, -1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, -2, .5}, {1, 2, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, 2, .5}, {-1, -1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, -1, .5}, {-1, 1, 0}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, 1, .5}, {-1, -1, 0}}, unit_box));
+  BOOST_TEST(intersects(Ray{{-1, 0, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 0, .5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 0, .5}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, 0, .5}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, 0, .5}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, 0, .5}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, -1, .5}, {1, 1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, 1, .5}, {1, -1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, -2, .5}, {1, 2, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, 2, .5}, {-1, -1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, -1, .5}, {-1, 1, 0}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, 1, .5}, {-1, -1, 0}}, unit_box1));
   // hit a corner
-  BOOST_TEST(intersects(Ray{{-1, 1, 1}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1, 1}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{-1, 1, 1}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, 1, 1}, {-1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, 1, 1}, {1, 0, 0}}, unit_box));
-  BOOST_TEST(!intersects(Ray{{2, 1, 1}, {0, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{-1, -1, -1}, {1, 1, 1}}, unit_box));
-  BOOST_TEST(intersects(Ray{{2, 2, 2}, {-1, -1, -1}}, unit_box));
+  BOOST_TEST(intersects(Ray{{-1, 1, 1}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1, 1}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{-1, 1, 1}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, 1, 1}, {-1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, 1, 1}, {1, 0, 0}}, unit_box1));
+  BOOST_TEST(!intersects(Ray{{2, 1, 1}, {0, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{-1, -1, -1}, {1, 1, 1}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{2, 2, 2}, {-1, -1, -1}}, unit_box1));
 
-  BOOST_TEST(!intersects(Ray{{1, 2, 3}, {4, 5, 6}}, unit_box));
-  BOOST_TEST(intersects(Ray{{1, 2, 3}, {-1, -2, -3}}, unit_box));
+  BOOST_TEST(!intersects(Ray{{1, 2, 3}, {4, 5, 6}}, unit_box1));
+  BOOST_TEST(intersects(Ray{{1, 2, 3}, {-1, -2, -3}}, unit_box1));
+
+  // box2
+  // origin is within the box
+  BOOST_TEST(intersects(Ray{{-.5, -.5, -.5}, {1, 0, 0}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{-.5, -.5, -.5}, {0, 1, 0}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{-.5, -.5, -.5}, {0, 0, 1}}, unit_box2));
+
+  // origin is outside the box
+  // hit the center of the face
+  BOOST_TEST(intersects(Ray{{1, -.5, -.5}, {-1, 0, 0}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{1, -.5, -.5}, {1, 0, 0}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{-.5, 1, -.5}, {0, -1, 0}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{-.5, 1, -.5}, {0, 1, 0}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{-.5, -.5, 1}, {0, 0, -1}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{-.5, -.5, 1}, {0, 0, 1}}, unit_box2));
+
+  // hit the center of an edge
+  BOOST_TEST(intersects(Ray{{.5, .5, -1}, {-1, -1, 0}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{.5, .5, -1}, {1, 1, 0}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{.5, -1, .5}, {-1, 0, -1}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{.5, -1, .5}, {1, 0, 1}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{-1, .5, .5}, {0, -1, -1}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{-1, .5, .5}, {0, 1, 1}}, unit_box2));
+
+  // hit a corner
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {-1, -1, -1}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{.5, .5, .5}, {1, 1, 1}}, unit_box2));
+  BOOST_TEST(intersects(Ray{{.5, .5, .5}, {-1, -1, -1}}, unit_box2));
+  BOOST_TEST(!intersects(Ray{{.5, .5, .5}, {1, 1, 1}}, unit_box2));
 }
